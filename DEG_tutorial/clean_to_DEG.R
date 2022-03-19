1ba498999808e1170f747dee9876fe5a9f880b79
naoto-hikawa <64478166+naoto-hikawa@users.noreply.github.com>
2022-03-19 18:17:37 +0900

    Update clean_to_DEG.R

--- DEG_tutorial/clean_to_DEG.R
+++ DEG_tutorial/clean_to_DEG.R
@@ -25,7 +25,7 @@ dds <- DESeq(dds,fitType='local')
 data_geneid <- rownames(c)
 
 # Place your downloaded gene_conversion_r6.44.csv in $SEQ_HOME
-conversion_table <- read.table("../../../../gene_conversion_r6.44.csv", 
+conversion_table <- read.table("gene_conversion_r6.44.csv", 
                                header=T, sep=",", quote="", stringsAsFactors=F)
 
 df_conversion = as.data.frame(matrix(ncol = 2, nrow = 0))
@@ -48,7 +48,7 @@ all_stats <- cbind(data_geneid, res)
 # order by FDR and save all stats information including non-DEGs
 resOrdered <- all_stats[order(all_stats$padj),]
 stats=paste(name, "_all_stats.txt", sep="")
-write.table(resOrdered, file=paste("deg_results/", stats, sep=""), sep="\t", append=F, quote=F, row.names=F)
+write.table(resOrdered, file=paste("results/", stats, sep=""), sep="\t", append=F, quote=F, row.names=F)
 
 # Keep all genes with FDR < 0.05 AND baseMean >10
 resSig <- subset(resOrdered, padj < 0.05) 
@@ -59,26 +59,26 @@ resSIG_FC <- subset(resSig, abs(log2FoldChange) > 0.58)
 # Write all DEGs to text and R data file
 DEG_res=paste(name, "_all_DEGs.txt", sep="")
 DEG_res_rda=paste(name, "_all_DEGs.rda", sep="")
-write.table(resSig, file=paste("deg_results/", DEG_res, sep=""), sep="\t", append=F, quote=F, row.names=F)
-save(resSig, file=paste("deg_results/", DEG_res_rda, sep=""))
+write.table(resSig, file=paste("results/", DEG_res, sep=""), sep="\t", append=F, quote=F, row.names=F)
+save(resSig, file=paste("results/", DEG_res_rda, sep=""))
 
 # Write DEGs with absolute Fold Change > 1.5
 sig_FC=paste(name, "_DEGs_FC.txt", sep="")
-write.table(resSIG_FC, file=paste("deg_results/", sig_FC, sep=""), sep="\t", append=F, quote=F, row.names=F)
+write.table(resSIG_FC, file=paste("results/", sig_FC, sep=""), sep="\t", append=F, quote=F, row.names=F)
 
 # Write DEGs with Fold Change > 1.5
 sig_FC_up=paste(name, "_DEGs_FC_up.txt", sep="")
-write.table(resSIG_FC_up, file=paste("deg_results/", sig_FC_up, sep=""), sep="\t", append=F, quote=F, row.names=F)
+write.table(resSIG_FC_up, file=paste("results/", sig_FC_up, sep=""), sep="\t", append=F, quote=F, row.names=F)
 
 # Write DEGs with inverse Fold Change > 1.5
 sig_FC_down=paste(name, "_DEGs_FC_down.txt", sep="")
-write.table(resSIG_FC_down, file=paste("deg_results/", sig_FC_down, sep=""), sep="\t", append=F, quote=F, row.names=F)
+write.table(resSIG_FC_down, file=paste("results/", sig_FC_down, sep=""), sep="\t", append=F, quote=F, row.names=F)
 
 # Create normalized count data file
 DESeq2_normalize <- counts(dds, normalized=TRUE) 
 DESeq2_normalize <- cbind(data_geneid, DESeq2_normalize)
 normalized_file=paste(name, "_normalized.txt", sep="")
-write.table(DESeq2_normalize, file=paste("deg_results/", normalized_file, sep=""), sep="\t", append=F, quote=F, row.names=F) 
+write.table(DESeq2_normalize, file=paste("results/", normalized_file, sep=""), sep="\t", append=F, quote=F, row.names=F) 
 
 # Create file suited for GSEA, filter DEGs only
 GSEA = data.frame(matrix(nrow=nrow(DESeq2_normalize), ncol = 0))
@@ -92,4 +92,4 @@ GSEASig <- subset(GSEA, padj < 0.05)
 GSEASig <- subset(GSEASig, baseMean >= 10)
 
 GSEA_DEG =paste(name, "_GSEA_DEG.txt", sep="")
-write.table(GSEASig[1:10], file=paste("deg_results/", GSEA_DEG, sep=""), sep="\t", append=F, quote=F, row.names=F)
+write.table(GSEASig[1:10], file=paste("results/", GSEA_DEG, sep=""), sep="\t", append=F, quote=F, row.names=F)
